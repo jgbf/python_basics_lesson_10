33383d0bef3071b4fd7349713c2a1d3bd7263579
mark <kemuaat@gamil.com>
2022-04-13 09:40:00 +0200

    Kapu es golszamlalo

--- skeleton.py
+++ skeleton.py
@@ -16,6 +16,9 @@ ball_h = 100
 ball_w = 100
 ball = pygame.transform.scale(ball, (ball_h, ball_w))
 
+kapu = pygame.image.load('kapu.png')
+kapu = pygame.transform.scale(kapu, (400, 200))
+
 font = pygame.font.Font('freesansbold.ttf', 48)
 text_rect = pygame.Rect(300, 0, 48, 300)
 
@@ -32,11 +35,19 @@ text = font.render('Ez egy foci palya', True, WHITE)
 pygame.key.set_repeat(1)
 
 def main():
+
+    gol_szam = 0
+    is_connected = False
+
     # Beallitjuk a kepkocka sebesseget, mert kulonbozo CPU-kon kolonbozo 
     # sebesseggel futhat a jatek
     clock = pygame.time.Clock()
 
     ball_rect = pygame.Rect(0, 0, ball_w, ball_w)
+    kapu_rect = kapu.get_rect()
+    kapu_rect.x = 300
+    kapu_rect.y = 600
+
 
     # Vegtelen ciklus, ami eletben tarja az ablakot, egyebkent rogton bezarodna 
     # az ablaak, amit fent letrehoztunk, mert veget erna a program
@@ -62,11 +73,25 @@ def main():
                 if event.key == K_s:
                     ball_rect.y += VEL
 
+        if kapu_rect.colliderect(ball_rect) and (is_connected == False):
+            gol_szam += 1
+            is_connected = True
+
+        if not kapu_rect.colliderect(ball_rect):
+            is_connected = False 
+
+        gol_szamlalo = font.render(f'A golok szama: {gol_szam}', True, WHITE)
+        gol_szamlalo_rect = gol_szamlalo.get_rect()
+        gol_szamlalo_rect.x = 300
+        gol_szamlalo_rect.y = 300
+
         # Beallitjuk a hatterer szinet
         WIN.fill(GREEN)
 
+        WIN.blit(kapu, kapu_rect)
         WIN.blit(ball, (ball_rect.x, ball_rect.y))
         WIN.blit(text, (text_rect.x, text_rect.y))
+        WIN.blit(gol_szamlalo, (gol_szamlalo_rect.x, gol_szamlalo_rect.y))
 
         # Frissitjuk a teljes kepernyot, hogy ha valtozna valami akkor az megjelenjen
         pygame.display.update()
